408f14167b7ed93a28bc10dea93560da79c559ac
Vineeshau <Vineesh.AU@chordify.com>
2023-02-21 15:16:53 +0530

    modified pgms

--- prime.rb
+++ prime.rb
@@ -1,21 +1,20 @@
-def prime
-	flag = 0
-	print "Enter the number : "
-	number = gets.chomp.to_i
-	if number == 0 || number == 1
-		puts "Not a prime number"
-	else
-		for i in 2..number/2 do
-			if number%i == 0
-				flag = 1
-				break
-			end
+flag = 0
+print "Enter the number : "
+number = gets.chomp.to_i
+if number == 0 || number == 1
+	puts "Not a prime number"
+else
+	for i in 2..number/2 do
+		if number%i == 0
+			flag = 1
+			break
 		end
+	end
 if flag == 0
 	puts "It's a prime number"
 else
 	puts "Not a prime number"
 	end
 end
-end
-prime
+
+
